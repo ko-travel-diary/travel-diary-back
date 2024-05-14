58c238b66a94a8443082e8dbcf6da9b60db848e3
Ksheon <rmldldldl0309@gmail.com>
2024-05-14 09:53:22 +0900

    2024-05-14 09:53

---
+++ src/main/java/com/traveldiary/back/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,102 @@
+package com.traveldiary.back.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.traveldiary.back.entity.UserEntity;
+import com.traveldiary.back.provider.JwtProvider;
+import com.traveldiary.back.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+/*
+▶ Spring Security Filter Chain에 추가할 JWT 필터
+- Request 객체로부터 Header 정보를 받아와서 Header에 있는 Authorization 필드의 Bearer 토큰 값을 가져와서 JWT 검증
+- 접근 주체의 권한을 확인하여 권한등록
+ */
+
+// OncePerRequestFilter 커스텀 필드를 생성하기 위해 확장 > 필수적으로 @Override
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+    // JwtAuthenticationFilter의 실제 동작
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        try {
+            
+            String token = parseBearerToken(request);
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String userId = jwtProvider.validate(token);
+            if (userId == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            UserEntity userEntity = userRepository.findByUserId(userId);
+            if (userEntity == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String role = userEntity.getUserRole();
+
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(role));
+
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null, authorities);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
+            
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if (!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if(!isBearer) return null;
+
+        String token = authorization.substring(7);
+        
+        return token;
+    }
+    
+}
