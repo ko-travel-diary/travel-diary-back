67f7da58f95433da1c07395bbf694b76ebaf9a5b
Young <anxis001220@gmail.com>
2024-06-04 01:48:12 +0900

    myList & search

--- src/main/java/com/traveldiary/back/repository/TravelReviewRepository.java
+++ src/main/java/com/traveldiary/back/repository/TravelReviewRepository.java
@@ -110,6 +110,27 @@ public interface TravelReviewRepository extends JpaRepository<TravelReviewEntity
             "GROUP BY travel_review_number " +
         ") i " +
         "ON r.review_number = i.travel_review_number " +
+        "WHERE r.review_writer_id = :userId  " +
+        "ORDER BY r.review_number DESC",
+        nativeQuery=true
+    )
+    List<GetTravelReviewResultSet>findByReviewWriterIdOrderByReviewNumberDesc(String userId);
+
+    @Query(
+        value=
+        "SELECT image, " + 
+                "r.review_number as reviewNumber, " +
+                "r.review_title as reviewTitle, " +
+                "r.review_content as reviewContent, " + 
+                "r.review_writer_id as reviewWriterId, " +
+                "r.review_datetime as reviewDatetime, " +
+                "r.review_view_count as reviewViewCount, " +
+                "r.review_favorite_count as reviewFavoriteCount FROM travel_review r LEFT JOIN ( " +
+            "SELECT travel_review_number, ANY_VALUE(travel_review_image_url) as image " +
+            "FROM travel_review_image " +
+            "GROUP BY travel_review_number " +
+        ") i " +
+        "ON r.review_number = i.travel_review_number " +
         "WHERE r.review_title LIKE %:searchWord%  " +
         "ORDER BY r.review_number DESC",
         nativeQuery=true
