18c52910da9ce18c274422dc81bbcb4123a7fbd3
DDDDKo <to1583@naver.com>
2024-06-12 17:29:46 +0900

    code fix

--- src/main/java/com/traveldiary/back/repository/UserRepository.java
+++ src/main/java/com/traveldiary/back/repository/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import com.traveldiary.back.entity.UserEntity;
 
-
-// estate 데이터베이스의 user 테이블의 작업을 위한 리포지토리 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String>{
 
